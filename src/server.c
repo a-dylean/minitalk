d4fe65cadb316adfae53185ac9f421d22c2bdc45
Alisa Tonkopiy <atonkopi@bess-f4r4s9.clusters.42paris.fr>
2024-01-30 15:29:53 +0100

    added signal handler

--- src/server.c
+++ src/server.c
@@ -6,27 +6,58 @@
 /*   By: atonkopi <atonkopi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 19:34:45 by atonkopi          #+#    #+#             */
-/*   Updated: 2024/01/30 14:17:06 by atonkopi         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:07:44 by atonkopi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
+#include <signal.h>
 
-void	signal_handler(int signum)
+void	signal_handler(int signum, siginfo_t *info, void *context)
 {
-	ft_printf("Received SIGINT %d!\n", signum);
-	exit(0);
+	static unsigned char	character = 0;
+	static int				bit_count = 0;
+	static __pid_t			client_pid = 0;
+
+	(void)context;
+	if (client_pid != info->si_pid)
+	{
+		bit_count = 0;
+		character = 0;
+	}
+	client_pid = info->si_pid;
+	character = character << 1;
+	if (signum == SIGUSR1)
+		character = character | 1;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(1, &character, 1);
+		bit_count = 0;
+		character = 0;
+	}
+	kill(client_pid, SIGUSR2);
 }
 int	main(void)
 {
 	__pid_t	pid;
+	struct sigaction sa;
 
+	sa.sa_handler = &signal_handler;
+	sa.sa_flags = 0;
 	pid = getpid();
 	ft_printf("Server pid: %d\n", pid);
-	// signal(SIGINT, signal_handler);
-	// while (1)
+	// if (sigaction(SIGINT, &sa, NULL) == -1)
 	// {
-	// 	ft_printf("hello");
-	// 	sleep(1);
+	// 	ft_printf("Error");
+	// 	exit(1);
 	// }
+	printf("Waiting for the client...\n");
+	// signal(SIGINT, signal_handler);
+	while (1)
+	{
+		//ft_printf("hello");
+		sleep(1);
+	}
+	return (0);
 }
